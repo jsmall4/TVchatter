0c01ddf237d0242194def2672936907f06959f91
Jack Smallwood <jacksmallwood@icloud.com>
2022-10-06 20:36:03 +0100

    added api folder to controllers and started index.js file

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const commentRoutes = require("./projectRoutes");
+const TVRoutes = require("./TVRoutes");
+
+router.use("/users", userRoutes);
+router.use("/projects", commentRoutes);
+router.use("/projects", TVRoutes);
+
+module.exports = router;
