45f8c6ef1073fdf397e84a977c5d6d6c8ad3626e
nxtera <64105557+nxtera@users.noreply.github.com>
2022-10-06 20:54:05 +0100

    Created index, tv and user models Creted seed data for tv shows and user
    On branch creatingDB
    Changes to be committed:
    	modified:   models/index.js
    	modified:   models/tvshow.js
    	modified:   models/user.js
    	new file:   seeds/tvshows-seeds.json
    	new file:   seeds/user-seeds.js

---
+++ seeds/user-seeds.js
@@ -0,0 +1,26 @@
+[
+  {
+    "username": "Mike",
+    "email": "mike@hotmail.com",
+    "password": "password12345",
+  },
+
+  {
+    "username": "Andy",
+    "email": "andy@hotmail.com",
+    "password": "password12345",
+  },
+
+  {
+    "username": "Kim",
+    "email": "kim@hotmail.com",
+    "password": "password12345",
+  },
+
+  {
+    "username": "Stacey",
+    "email": "stacey@hotmail.com",
+    "password": "password12345",
+  }
+  
+];
