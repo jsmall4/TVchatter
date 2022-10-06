0089ea8d41def4a98454af745a80a24d65cf08e2
Jack Smallwood <jacksmallwood@icloud.com>
2022-10-06 18:39:39 +0100

    const sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,15 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = process.env;
+const sequelize = process.env. {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3001,
+}
+
+
 
 // PORT, DB_PW, DB_NAME, host?
+
 
 module.exports = sequelize;
