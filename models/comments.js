6d0f4b432ab5e4756567671344d18fb0ce65222e
nxtera <64105557+nxtera@users.noreply.github.com>
2022-10-06 21:24:27 +0100

    Created comments models Updated index model and established relashionships between models.
    On branch creatingDB
    Changes to be committed:
    	deleted:    models/comment.js
    	new file:   models/comments.js
    	modified:   models/index.js

---
+++ models/comments.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+// create our Comments model
+class Comments extends Model {}
+// create fields/columns for comments model
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tvshow",
+  }
+);
+module.exports = Comments;
