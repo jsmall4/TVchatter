2252b5a53a7bbf4fbc057aec4e0074fc03d7e169
Laura Kambanis <laurakambanis@Lauras-Air-3.home>
2022-10-10 21:22:19 +0100

    created sign-up page handle bar and homepage route, and sign up route and dashboard route.

--- controllers/routes/tvshows.js
+++ controllers/routes/tvshows.js
@@ -1,10 +1,9 @@
-const tvshows = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { router } = require('.');
+// const tvshows = require("express").Router();
+// const { v4: uuidv4 } = require("uuid");
+// const { router } = require(".");
 
-
-router.get('/tvshow/:id', (req, res) => {
-    //1. api call to endpoint to get the movieData
-    //2. fetch comments from db by using where movie_id === req.params.id and user_id===req.session.user_id
-    // req.render("movie_page", {movieData, comments})
-  });
+// router.get("/tvshow/:id", (req, res) => {
+//   1. api call to endpoint to get the movieData
+//   2. fetch comments from db by using where movie_id === req.params.id and user_id===req.session.user_id
+//   req.render("movie_page", {movieData, comments})
+// });
--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const sequelize = require("./config/connection");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+// const sess = {
+//   secret: "Super secret secret",
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
