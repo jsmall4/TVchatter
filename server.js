0e9d4508ccab280eab967926c5cbe7b0f3cc15f5
Laura Kambanis <kambanislaura@gmail.com>
2022-10-11 21:15:08 +0100

    changed css and solved carousel issue, added bootstrap

--- server.js
+++ server.js
@@ -23,17 +23,14 @@ const sess = {
 
 app.use(session(sess));
 
-
-
-
-
 const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(routes);
 
