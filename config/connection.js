4e661e78469a08fa6aca96b3f41c1e6637d98a8f
Laura Kambanis <kambanislaura@gmail.com>
2022-10-16 15:09:36 +0100

    changed host environment

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: "localhost",
+      host: process.env.DB_HOST || "localhost",
       dialect: "mysql",
       port: 3306,
     }
