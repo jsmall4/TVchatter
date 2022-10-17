9687bb161cecbed09ad871b858f2f9fe28786a7c
jsmall4 <64646369+jsmall4@users.noreply.github.com>
2022-10-17 18:43:33 +0100

    emergency addition

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,23 +3,23 @@ const { User, Comments } = require("../models");
 const withAuth = require("../utils/auth");
 
 //render dashboard page
-router.get("/", withAuth, (req, res) => {
-  Comments.findAll({
-    where: {
-      user_id: req.session.user_id,
-    },
-    attributes: ["id", "user_comment", "tvshow_id"],
-  })
-    .then((dbCommentData) => {
-      const comments = dbCommentData.map((post) => post.get({ plain: true }));
-      console.log(comments);
-      res.render("dashboard", { comments, loggedIn: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get("/", withAuth, (req, res) => {
+//   Comments.findAll({
+//     where: {
+//       user_id: req.session.user_id,
+//     },
+//     attributes: ["id", "user_comment", "tvshow_id"],
+//   })
+//     .then((dbCommentData) => {
+//       const comments = dbCommentData.map((post) => post.get({ plain: true }));
+//       console.log(comments);
+//       res.render("dashboard", { comments, loggedIn: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 router.get("/edit/:id", withAuth, (req, res) => {
   Comments.findByPk(req.params.id, {
